759116c85a08cee341f9744e017238bac1069e1f
Denis <fixprogram@mail.ru>
2021-10-17 12:02:12 +0300

    Testing our exchange by seed js script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,15 @@
 // migrations are changing the state of blockchain
 
 const Token = artifacts.require("Token");
+const Exchange = artifacts.require("Exchange");
 
-module.exports = function (deployer) {
-  deployer.deploy(Token);
+module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts();
+
+  await deployer.deploy(Token);
+
+  const feeAccount = accounts[0];
+  const feePercent = 10;
+
+  await deployer.deploy(Exchange, feeAccount, feePercent);
 };
